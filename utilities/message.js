f061b5a4a2e38df6267ef420caf5a311d511d24e
Stipendi <33043114+Stipendi@users.noreply.github.com>
2019-03-10 15:57:29 +0200

    added comments

--- utilities/message.js
+++ utilities/message.js
@@ -5,16 +5,15 @@ module.exports = new class Message {
   async create(channel, content) { // this will offer more flexibility later on
     return await channel.createMessage(content);
   }
-  formatQuestion(triviaQuestion) {
-    console.log(triviaQuestion);
+  formatQuestion(triviaQuestion) { // make an embed out of a question
     let answers = triviaQuestion.answers.map((ans, i) => {
       return { content: ans, right: triviaQuestion.right_answers[i] };
-    });
+    }); // give a true/false flag to each question
     return new Embed(triviaQuestion.question, answers.map((ans, i) => {
-      return this.bold(String.fromCharCode(97 + i).toUpperCase() + ") ") + ans.content + " (" + ans.right + ")";
+      return this.bold(String.fromCharCode(97 + i).toUpperCase() + ") ") + ans.content + " (" + ans.right + ")"; // format the answer
     }).join("\n"));
   }
-  bold(string) {
+  bold(string) { // make a string bolded on Discord
     return "**" + string + "**";
   }
 }();
